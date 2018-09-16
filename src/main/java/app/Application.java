ee8b12feb780cb23d811442443377f0ce47b2ae3
OmieWalls <omie.walls@daugherty.com>
2018-09-16 13:15:28 -0400

    Spring launcher class added

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,25 +3,10 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-//    @Bean
-//    public PubSubInboundChannelAdapter messageChannelAdapter(
-//            @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
-//            PubSubOperations pubSubTemplate) {
-//        PubSubInboundChannelAdapter adapter =
-//                new PubSubInboundChannelAdapter(pubSubTemplate, "testSubscription");
-//        adapter.setOutputChannel(inputChannel);
-//        adapter.setAckMode(AckMode.MANUAL);
-//
-//        return adapter;
-//    }
-
 }
